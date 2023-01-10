0d429bc7b78fcc4ca687de80e3319ffb96f2c4d2
BlvkcAres <xzchauke@protonmail.me>
2023-01-10 21:10:24 +0200

    hotfix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
 # Creating a file
-
 file { 'school':
-ensure => 'present',
 content => 'I love Puppet',
-owner => 'www-data',
-group => 'www-data',
-path => '/tmp/school',
-mode => '0744'
+owner   => 'www-data',
+group   => 'www-data',
+path    => '/tmp/school',
+mode    => '0744'
 }
